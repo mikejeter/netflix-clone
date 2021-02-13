9b20cc7e50df6e9d531cdfe1691d54b295446d1c
mikejeter <mjeter45@yahoo.com>
2021-02-12 22:43:25 -0500

    Created Rows and axios GET useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import request from './request';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey Clever Programmer! Let's build Netflix Clone Front-End today</h1>
+      <h1>
+        Hey Clever Programmer! Let's build Netflix Clone Front-End today
+      </h1>
+      <Row title="NETFLIX ORIGINALS"
+       fetchUrl={request.fetchNetflixOriginals}
+       isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
